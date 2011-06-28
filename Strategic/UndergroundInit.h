4637de11ca5406d3dad1e6b05a4cfc4680e66fe1
jazz_ja <jazz_ja@b41f55df-6250-4c49-8e33-4aa727ad62a1>
2011-06-28 16:54:03 +0000

    *Old source code was removed.

--- Strategic/UndergroundInit.h
+++
@@ -1,9 +0,0 @@
-#ifndef _UNDERGROUNDINIT_H
-#define _UNDERGROUNDINIT_H
-
-#include "Types.h"
-
-extern BOOLEAN LetLuaBuildUndergroundSectorInfoList();
-extern BOOLEAN locationStringToCoordinates(std::string loc, UINT8* x, UINT8* y, UINT8* z);
-
-#endif
